b25e46c9abb9f727615d6ab986cf778efd19b6ca
soumya git hub <soumyaranjanpanda13@gmail.com>
2019-08-24 17:56:53 +0530

    code fixed

--- src/main/java/Hybridfrm/Hybridfrm/App.java
+++ src/main/java/Hybridfrm/Hybridfrm/App.java
@@ -2,6 +2,9 @@ package Hybridfrm.Hybridfrm;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 /**
@@ -18,26 +21,30 @@ public class App
     public void Start()
     {
     	Url=System.getProperty("Url");
+    	System.out.println(Url);
     	Browser=System.getProperty("Browser");
+    	System.out.println(Browser);
     	if(Browser.equalsIgnoreCase("Chrome")) {
     		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\eclipse-workspace\\Hybridfrm\\Resource\\chromedriver.exe");
         	driver=new ChromeDriver();
     	}else if(Browser.equalsIgnoreCase("IE")) {
+    		DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+    		caps.setCapability("ignoreZoomSetting", true);
     		System.setProperty("webdriver.ie.driver", "C:\\Users\\admin\\eclipse-workspace\\Hybridfrm\\Resource\\IEDriverServer.exe");
-        	driver=new ChromeDriver();
+        	driver=new InternetExplorerDriver(caps);
     	}else if(Browser.equalsIgnoreCase("Firefox")) {
     		System.setProperty("webdriver.gecko.driver", "C:\\Users\\admin\\eclipse-workspace\\Hybridfrm\\Resource\\geckodriver.exe");
-        	driver=new ChromeDriver();
+        	driver=new FirefoxDriver();
     	}
-    	
 
-		if(Url.equalsIgnoreCase("Google")) {
+    	if(Url.equalsIgnoreCase("Google")) {
 			driver.get("https://www.google.co.in");
 			System.out.println("https://www.google.co.in");
 		}else if(Url.equalsIgnoreCase("Facebook")) {
 			driver.get("https://www.facebook.com");
 			System.out.println("https://www.facebook.com");
 		}
+
     }
     
     
