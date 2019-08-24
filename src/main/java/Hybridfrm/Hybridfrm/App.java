911bae21bed0da51cac6e0a8c0dfb1a0d0d6709a
soumya git hub <soumyaranjanpanda13@gmail.com>
2019-08-24 17:40:59 +0530

    new requirement

--- src/main/java/Hybridfrm/Hybridfrm/App.java
+++ src/main/java/Hybridfrm/Hybridfrm/App.java
@@ -12,19 +12,31 @@ public class App
 {
 	WebDriver driver;
 	String Url;
+	String Browser;
     @Test
     public void Start()
     {
     	Url=System.getProperty("Url");
-    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\eclipse-workspace\\Hybridfrm\\Resource\\chromedriver.exe");
-    	driver=new ChromeDriver();
-    			if(Url.equalsIgnoreCase("Google")) {
-    				driver.get("https://www.google.co.in");
-    				System.out.println("https://www.google.co.in");
-    			}else if(Url.equalsIgnoreCase("Facebook")) {
-    				driver.get("https://www.facebook.com");
-    				System.out.println("https://www.facebook.com");
-    			}
+    	Browser=System.getProperty("Browser");
+    	if(Browser.equalsIgnoreCase("Chrome")) {
+    		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\eclipse-workspace\\Hybridfrm\\Resource\\chromedriver.exe");
+        	driver=new ChromeDriver();
+    	}else if(Browser.equalsIgnoreCase("IE")) {
+    		System.setProperty("webdriver.ie.driver", "C:\\Users\\admin\\eclipse-workspace\\Hybridfrm\\Resource\\IEDriverServer.exe");
+        	driver=new ChromeDriver();
+    	}else if(Browser.equalsIgnoreCase("Firefox")) {
+    		System.setProperty("webdriver.gecko.driver", "C:\\Users\\admin\\eclipse-workspace\\Hybridfrm\\Resource\\geckodriver.exe");
+        	driver=new ChromeDriver();
+    	}
+    	
+
+		if(Url.equalsIgnoreCase("Google")) {
+			driver.get("https://www.google.co.in");
+			System.out.println("https://www.google.co.in");
+		}else if(Url.equalsIgnoreCase("Facebook")) {
+			driver.get("https://www.facebook.com");
+			System.out.println("https://www.facebook.com");
+		}
     }
     
     
